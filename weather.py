bea04da107ca1f4ad90b8b99bf406f4e5fbdfaf1
Madelene Campos <madelenecampos@gmail.com>
2023-01-13 18:25:20 +0000

    Grabs data from Open Weather Map API and displays it in F and C via a simple Flask app

---
+++ weather.py
@@ -0,0 +1,28 @@
+import requests
+import json
+from datetime import datetime 
+
+from flask import Flask
+
+app = Flask(__name__)
+
+# From OpenWeatherMap https://home.openweathermap.org/api_keys
+# Add your own API key
+api_key = ""
+
+#lat and lon of Bristol, UK
+lat = "51.454514"
+lon = "-2.587910"
+
+# API URL (f strings wouldn't work?)
+weather_url = "http://api.openweathermap.org/data/2.5/weather?lat={}&lon={}&appid={}&units=imperial".format(lat, lon, api_key)
+
+response = requests.get(weather_url)
+weather_info = response.json()
+
+@app.route("/")
+def bristol_weather():
+    if weather_info["cod"] == 200:
+        celcius = weather_info["main"]["temp"] - 32 * 5/9
+        weather = "The weather, right now in Bristol, is {} F and {} C".format(weather_info["main"]["temp"], celcius)
+        return "<p>{}</p>".format(weather)
